034532aba9ca0b8da205d4097b2c5986013d5344
thiagomoreirasantos <tmoreiradossantos733@gmail.com>
2023-05-13 15:24:15 +0100

    First Release

---
+++ src/Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open Deedle
+
+let root = __SOURCE_DIRECTORY__
+let premierL2020 = Frame.ReadCsv(root + "./dataset/2020s/2020-21/eng.5.csv")
+
+
+

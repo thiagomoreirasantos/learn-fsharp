f3b3a70173f7ad78e912babf08a4711666eccd6f
Thiago Moreira <ext.thiago.moreira@farfetch.com>
2023-05-14 22:34:53 +0100

    Change name of filter

--- src/Program.fs
+++ src/Program.fs
@@ -7,8 +7,8 @@ let root = __SOURCE_DIRECTORY__
 
 let PremierLeague = new CsvProvider<"./dataset/2020s/2020-21/eng.1.csv", ResolutionFolder=root>()
 
-let mean = PremierLeague.Filter(fun row -> row.``Team 1`` = "Arsenal FC" || row.``Team 2`` = "Arsenal FC")
+let plfilter = PremierLeague.Filter(fun row -> row.``Team 1`` = "Arsenal FC" || row.``Team 2`` = "Arsenal FC")
 
-for row in mean.Rows |> Seq.truncate 10 do
+for row in plfilter.Rows |> Seq.truncate 10 do
     printfn "Match: %s,%s,%s"  row.``Team 1``  row.FT row.``Team 2``
     printfn "Date: %s" (row.Date.ToString "yyyy/MM/dd")
